a1610df1821a4b8815b1895086bb037464157ced
sohelrana3 <expartdesigns5@gmail.com>
2023-04-02 23:11:55 +0600

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import Container from './leyouts/Container'
+import Flex from './leyouts/Flex'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <Container>
+      <Flex className='flex justify-between'>
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+      </Flex>
+    </Container>
   )
 }
 
