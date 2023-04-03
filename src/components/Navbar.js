3fbb4e98f754ac2a2d737c3d81da5e92c63ce61f
sohelrana3 <expartdesigns5@gmail.com>
2023-04-04 00:14:13 +0600

    home Catalog

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,14 +4,17 @@ import Flex from './leyouts/Flex'
 import List from './leyouts/List'
 import {FaAngleDown} from 'react-icons/fa'
 import Listitem from './leyouts/Listitem'
-import Navitem from './leyouts/Navitem'
 
 
 const Navbar = () => {
   let [categories, setcatagorise] = useState(false) 
+  let [home, sethome] = useState(false)
+  let [Catalog, setCatalog] = useState(false)
   let handleclick = ()=>{
+    console.log("pppp");
     setcatagorise(!categories)
   }
+ 
   return (
     <div className='w-full bg-[#F4f4F4]'>
      <Container>
@@ -36,7 +39,40 @@ const Navbar = () => {
             </div>
         </div>
         <div>
-           <Navitem />
+          <List className='flex gap-x-3'>
+              <div onClick={()=> sethome(!home)} className='flex items-center gap-x-4 relative'>
+                <Listitem className='text-[#3A3A3A] text-base font-medium font-popi' listitem='Home' />
+                <FaAngleDown />
+                <div className='absolute top-10 left-0 bg-[#F4F4F4] w-full'>
+                  {
+                    home &&(
+                      <List>
+                      <Listitem href='/' className='text-[#3A3A3A] text-base font-medium font-popi py-2 border-b border-yow' listitem='Home' />
+                      <Listitem href='/' className='text-[#3A3A3A] text-base font-medium font-popi py-2 border-b border-yow' listitem='Home 2' />
+                      <Listitem href='/' className='text-[#3A3A3A] text-base font-medium font-popi py-2 border-b border-yow' listitem='Home 3' />
+                      </List>
+                    )
+                  }
+                </div>
+              </div>
+              <div onClick={()=> setCatalog(!Catalog)} className='flex items-center gap-x-4 relative'>
+                <Listitem className='text-[#3A3A3A] text-base font-medium font-popi' listitem='Catalog' />
+                <FaAngleDown />
+                <div className='absolute top-10 left-0 bg-[#F4F4F4] w-full'>
+                  {
+                    Catalog &&(
+                      <List>
+                      <Listitem href='/' className='text-[#3A3A3A] text-base font-medium font-popi py-2 border-b border-yow' listitem='Catalog' />
+                      <Listitem href='/' className='text-[#3A3A3A] text-base font-medium font-popi py-2 border-b border-yow' listitem='Catalog 2' />
+                      <Listitem href='/' className='text-[#3A3A3A] text-base font-medium font-popi py-2 border-b border-yow' listitem='Catalog 3' />
+                      </List>
+                    )
+                  }
+                </div>
+              </div>
+              
+          </List >
+             
         </div>
         <div>
           <h3 className='text-base text-primary font-bold font-popi'>30 Days Free Return</h3>
