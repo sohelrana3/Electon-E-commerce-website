3e4d75b1696d67efc27b4edbf0290cf25b1213a3
sohelrana3 <expartdesigns5@gmail.com>
2023-04-03 22:17:19 +0600

    catagoris click

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Container from './leyouts/Container'
 import Flex from './leyouts/Flex'
 import List from './leyouts/List'
@@ -7,27 +7,35 @@ import Listitem from './leyouts/Listitem'
 
 
 const Navbar = () => {
+  let [categories, setcatagorise] = useState(false) 
+  let handleclick = ()=>{
+    setcatagorise(!categories)
+  }
   return (
     <div className='w-full bg-[#F4f4F4]'>
      <Container>
       <Flex className='flex justify-between items-center'>
         <div className='relative'>
-          <p className='flex items-center gap-x-4 bg-yow py-4 px-6 text-base font-medium font-popi text-white cursor-pointer'>
+          <p onClick={handleclick} className='flex items-center gap-x-4 bg-yow py-4 px-6 text-base font-medium font-popi text-white cursor-pointer'>
             <span>Browse categories</span>
             <FaAngleDown />
+            </p>
             <div className='bg-yow absolute top-14 left-0 w-full '>
-              <List>
-                <Listitem href='avf' className='text-sm font-medium font-popi py-2 px-6 border-t border-[#F4f4F4] hover:px-8' listitem='Ectronics Devices' />
-                <Listitem href='avf' className='text-sm font-medium font-popi py-2 px-6 border-t border-[#F4f4F4] hover:px-8' listitem='Health & Beauty' />
-                <Listitem href='avf' className='text-sm font-medium font-popi py-2 px-6 border-t border-[#F4f4F4] hover:px-8' listitem='Watches, Bags' />
-                <Listitem href='avf' className='text-sm font-medium font-popi py-2 px-6 border-t border-[#F4f4F4] hover:px-8' listitem='Home & Lifestyle' />
-                <Listitem href='avf' className='text-sm font-medium font-popi py-2 px-6 border-t border-[#F4f4F4] hover:px-8' listitem='TV & Home Appliances' />
-              </List>
+              {
+                categories &&(
+                  <List>
+                    <Listitem href='avf' className='text-white text-sm font-medium font-popi py-2 px-6 border-t border-[#F4f4F4] hover:px-8' listitem='Ectronics Devices' />
+                    <Listitem href='avf' className='text-white text-sm font-medium font-popi py-2 px-6 border-t border-[#F4f4F4] hover:px-8' listitem='Health & Beauty' />
+                    <Listitem href='avf' className='text-white text-sm font-medium font-popi py-2 px-6 border-t border-[#F4f4F4] hover:px-8' listitem='Watches, Bags' />
+                    <Listitem href='avf' className='text-white text-sm font-medium font-popi py-2 px-6 border-t border-[#F4f4F4] hover:px-8' listitem='Home & Lifestyle' />
+                    <Listitem href='avf' className='text-white text-sm font-medium font-popi py-2 px-6 border-t border-[#F4f4F4] hover:px-8' listitem='TV & Home Appliances' />
+                </List>
+                )
+              }
             </div>
-          </p>
         </div>
         <div>
-
+              
         </div>
         <div>3</div>
       </Flex>
