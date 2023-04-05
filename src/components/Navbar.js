071e7d23904bd699669dfc3727770707c65b0ae4
Sohel Rana <89797602+sohelrana3@users.noreply.github.com>
2023-04-06 00:07:10 +0600

    Revert "navbar ueseffect"

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import Container from './leyouts/Container'
 import Flex from './leyouts/Flex'
 import List from './leyouts/List'
@@ -16,17 +16,7 @@ const Navbar = () => {
     console.log("pppp");
     setcatagorise(!categories)
   }
-  useEffect(()=>{
-    function scrollWidth(){
-        if(window.innerWidth < 1024){
-          setnavbar(false)
-        }else{
-          setnavbar(true)
-        }
-    }
-    scrollWidth()
-    window.addEventListener("resize", scrollWidth);
-},[])
+ 
   return (
     <div className='w-full bg-[#F4f4F4]'>
      <Container>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,10 +3,8 @@ import Banner from '../components/Banner'
 
 const Home = () => {
   return (
-    <>
-      <Banner />
-    </>
-    
+     // <Banner />
+    <></>
   )
 }
 
